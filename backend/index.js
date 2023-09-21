793fc201c79c0a6e9ef3545e0157fc9e1d750cb7
Zubaer-Alam <xuvaer@gmail.com>
2023-09-21 21:18:32 +0600

    ✔️products API

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const db = require("mongoose");
-const Product = require('./models/product.model')
+const Product = require("./models/product.model");
 
 const app = express();
-
 const port = 3000;
 
 db.connect("mongodb://localhost:27017/products", {
@@ -13,5 +12,17 @@ db.connect("mongodb://localhost:27017/products", {
 });
 
 app.use(bodyParser.json());
+
+app.post("/api/products", async (req, res) => {
+  try {
+    const productData = req.body;
+    const product = new Product(productData);
+    await product.save();
+    res.status(201).json(product);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to save the product." });
+  }
+});
 
 app.listen(port, () => console.log("server running"));
