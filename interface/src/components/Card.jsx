72cdd801a0217b21f44ea6f416997f187f8c3242
Zubaer-Alam <xuvaer@gmail.com>
2023-09-23 08:58:18 +0600

    ✔️Add Stock Cards

---
+++ interface/src/components/Card.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function ProductCard({ product }) {
+  return (
+    <div className="w-full mb-4">
+      <div className="bg-white border-2 border-black rounded-lg shadow-lg p-4 flex flex-wrap">
+        <div className="w-2/3 pr-4"> 
+          <h2 className="text-2xl font-semibold mb-3">
+            Product Name: {product.name}
+          </h2>
+          <h1 className="text-2xl font-semibold mb-3">
+            Product ID: {product["product-id"]}
+          </h1>
+        </div>
+        <div className="w-1/3 flex flex-wrap">
+          {product.variations.map((variation, index) => (
+            <div key={index} className="mb-4 w-1/2 p-2 border-2 rounded-lg"> 
+              <p className="text-lg font-medium mb-2">
+                Size: {variation.size}
+              </p>
+              <p className="text-lg font-medium mb-2">
+                Quantity: {variation.quantity}
+              </p>
+              <p className="text-lg font-medium mb-2">
+                Price: {variation.price}BDT
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
--- interface/src/components/Navbar.jsx
+++ interface/src/components/Navbar.jsx
@@ -1,29 +1,37 @@
-"use client";
-
-import { Tabs, Button } from "flowbite-react";
+import React, { useEffect, useState } from "react";
+import { Tabs } from "flowbite-react";
 import { HiClipboardList, HiTable, HiUserGroup } from "react-icons/hi";
 import ProductForm from "./Forms/ProductEntry";
-const Navbar = () => {
+import ProductCard from "./Card"; 
+
+export default function Navbar() {
+  const [productData, setProductData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/products")
+      .then((response) => response.json())
+      .then((data) => setProductData(data))
+      .then(console.log(productData))
+      .catch((error) => console.error("Error fetching product data:", error));
+  }, []);
+
   return (
-    <div>
-      <div className="justify-center">
-        <Button color="failure">Logout</Button>
-      </div>
-      <Tabs.Group aria-label="Tabs with icons" style="underline">
-        <Tabs.Item active icon={HiUserGroup} title="Users">
-          <p>USER MANAGEMENT</p>
-        </Tabs.Item>
+    <Tabs.Group aria-label="Tabs with icons" style="underline">
+      <Tabs.Item active icon={HiUserGroup} title="Users">
+        <p>USER MANAGEMENT</p>
+      </Tabs.Item>
 
-        <Tabs.Item icon={HiTable} title=" Stock">
-          <p>GET /api/products</p>
-        </Tabs.Item>
+      <Tabs.Item icon={HiTable} title="Stock">
+        <div className="flex flex-wrap">
+          {productData.map((product, index) => (
+            <ProductCard key={index} product={product} />
+          ))}
+        </div>
+      </Tabs.Item>
 
-        <Tabs.Item icon={HiClipboardList} title="Entry">
-          <ProductForm />
-        </Tabs.Item>
-      </Tabs.Group>
-    </div>
+      <Tabs.Item icon={HiClipboardList} title="Entry">
+        <ProductForm />
+      </Tabs.Item>
+    </Tabs.Group>
   );
-};
-
-export default Navbar;
+}
