b15a2ad9e72a5946b39005b8d7fca063d99a33f4
Zubaer-Alam <xuvaer@gmail.com>
2023-09-25 03:12:51 +0600

    📋Form validation

--- interface/src/components/Forms/ProductEntry.jsx
+++ interface/src/components/Forms/ProductEntry.jsx
@@ -1,6 +1,5 @@
-import "../../App.css";
-import React, { useState } from "react";
-import VariationForm from "../VariationForm";
+import React, { useState, useEffect } from "react";
+import VariationForm from "./VariationForm";
 
 const ProductForm = () => {
   const [variations, setVariations] = useState([
@@ -11,6 +10,10 @@ const ProductForm = () => {
       price: "",
     },
   ]);
+
+  const [productID, setProductID] = useState("");
+  const [productName, setProductName] = useState("");
+  const [formValid, setFormValid] = useState(false);
 
   const send = async (data) => {
     try {
@@ -52,9 +55,6 @@ const ProductForm = () => {
   const handleAdd = async (e) => {
     e.preventDefault();
 
-    const productId = e.target["product-id"].value;
-    const productName = e.target["product-name"].value;
-
     const variationsData = variations.map((variation) => ({
       variation: variation.variation,
       size: variation.size,
@@ -63,7 +63,7 @@ const ProductForm = () => {
     }));
 
     const data = {
-      "product-id": productId,
+      "product-id": productID,
       name: productName,
       variations: variationsData,
     };
@@ -80,65 +80,85 @@ const ProductForm = () => {
       },
     ]);
   };
+  
+
+  useEffect(() => {
+    const isFormValid =
+      productID.trim() !== "" &&
+      productName.trim() !== "" &&
+      variations.every(
+        (variation) =>
+          variation.variation.trim() !== "" &&
+          variation.size.trim() !== "" &&
+          variation.quantity.trim() !== "" &&
+          variation.price.trim() !== ""
+      );
+    setFormValid(isFormValid);
+  }, [productID, productName, variations]);
+
+
 
   return (
-    
-      <form
-        onSubmit={handleAdd}
-        className="text-black md:w-3/5 lg:w-3/5 mx-auto  border-2 border-black   flex flex-col justify-center  my-20  rounded-lg"
-      >
-        <div className="flex flex-col gap-4 m-10">
-          <div className="sm:flex justify-between gap-4">
-            <label className="font-semibold text-xl">Product ID:</label>
-            <input
-              className="bg-gray-200 rounded-md text-xl"
-              type="text"
-              name="product-id"
-            />
-          </div>
-          <div className="md:flex justify-between gap-4">
-            <label className="font-semibold text-xl">Product Name:</label>
-            <input
-              className=" rounded-md text-xl bg-gray-200"
-              type="text"
-              name="product-name"
-              id="product-name"
-            />
-          </div>
-        </div>
-
-        <button
-          className="mx-auto p-2 rounded-lg bg-blue-600 transition-transform hover:scale-105 text-white font-bold text-l"
-          type="button"
-          onClick={addVariation}
-        >
-          Add Variation
-        </button>
-
-        {variations.map((variation, index) => (
-          <VariationForm
-            key={index}
-            removeVariation={
-              variations.length > 1 ? () => removeVariation(index) : undefined
-            }
-            onUpdate={(updatedVariation) => {
-              const updatedVariations = [...variations];
-              updatedVariations[index] = updatedVariation;
-              setVariations(updatedVariations);
-            }}
-            variationData={variation}
+    <form
+      onSubmit={handleAdd}
+      className="text-black md:w-3/5 lg:w-3/5 mx-auto border-2 border-black flex flex-col justify-center my-20 rounded-lg"
+    >
+      <div className="flex flex-col gap-4 m-10">
+        <div className="sm:flex justify-between gap-4">
+          <label className="font-semibold text-xl">Product ID:</label>
+          <input
+            className="bg-gray-200 rounded-md text-xl"
+            type="text"
+            name="product-id"
+            value={productID}
+            onChange={(e) => setProductID(e.target.value)}
           />
-        ))}
+        </div>
+        <div className="md:flex justify-between gap-4">
+          <label className="font-semibold text-xl">Product Name:</label>
+          <input
+            className="rounded-md text-xl bg-gray-200"
+            type="text"
+            name="product-name"
+            id="product-name"
+            value={productName}
+            onChange={(e) => setProductName(e.target.value)}
+          />
+        </div>
+      </div>
 
-        <button
-          className="m-10 p-4 border-1 rounded-lg bg-green-600 transition-transform hover:scale-105 text-white font-bold text-xl"
-          type="submit"
-        >
-          ADD PRODUCT
-        </button>
-      </form>
-    
+      <button
+        className="mx-auto p-2 rounded-lg bg-blue-600 transition-transform hover:scale-105 text-white font-bold text-l"
+        type="button"
+        onClick={addVariation}
+      >
+        Add Variation
+      </button>
+
+      {variations.map((variation, index) => (
+        <VariationForm
+          key={index}
+          removeVariation={variations.length > 1 ? () => removeVariation(index) : undefined}
+          onUpdate={(updatedVariation) => {
+            const updatedVariations = [...variations];
+            updatedVariations[index] = updatedVariation;
+            setVariations(updatedVariations);
+          }}
+          variationData={variation}
+        />
+      ))}
+
+      <button
+        className={`m-10 p-4 rounded-lg bg-green-600 transition-transform hover:scale-105 text-white font-bold text-xl ${
+          !formValid && "opacity-50 cursor-not-allowed"
+        }`}
+        type="submit"
+        disabled={!formValid}
+      >
+        ADD PRODUCT
+      </button>
+    </form>
   );
-}
+};
 
 export default ProductForm;
